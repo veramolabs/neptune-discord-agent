220853abcff3efbceb819dde3fd6d12fba9243c3
Simonas Karuzas <simonas@not.cat>
2021-09-24 15:51:04 +0300

    feat: rename to resume

--- src/discord/commands/resume.ts
+++ src/discord/commands/resume.ts
@@ -7,13 +7,19 @@ import Debug from 'debug'
 const debug = Debug('discord:kudos')
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('credentials').setDescription('List my verifiable credentials'),
+  data: new SlashCommandBuilder().setName('resume').setDescription('Show members\'s resume')
+  .addUserOption((option) =>
+    option.setName('member').setDescription('Credential subject').setRequired(false),
+  ),
   async execute(interaction: CommandInteraction, agent: ConfiguredAgent) {
     const { user, client } = interaction
 
+    const recipient = interaction.options.getUser('member')
+    const memberId = recipient ? recipient.id : user.id
+
     const holder = await agent.didManagerGetOrCreate({
       provider: 'did:ethr',
-      alias: user.id,
+      alias: memberId,
     })
 
     const credentials = await agent.dataStoreORMGetVerifiableCredentials({
@@ -24,9 +30,16 @@ module.exports = {
       getMessageEmbedFromVC(verifiableCredential, true),
     )
 
-    await interaction.reply({
-      embeds,
-      ephemeral: true,
-    })
+    if (embeds.length > 0) {
+      await interaction.reply({
+        embeds,
+        ephemeral: true,
+      })
+    } else {
+      await interaction.reply({
+        content: 'You don\'t have any credentials issued to you',
+        ephemeral: true,
+      })
+    }
   },
 } as CommandHandler
